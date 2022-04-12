2bd67936064117f49a2e8965e357daf8b9364d62
kisielewskik <k.kisielewski.k@gmail.com>
2022-04-12 12:02:06 +0200

    feat: home page

--- src/components/layouts/app/app-container/app-container-styled.js
+++ src/components/layouts/app/app-container/app-container-styled.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const StyledAppContainer = styled.div``;
 
 export const Main = styled.main`
-  min-height: calc( 100vh - 150px );
-  }
+  min-height: calc(100vh - 150px);
+  position: relative;
 `;
---
+++ src/views/home/home.styled.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import { Input } from 'antd';
+
+export const HomeWrapper = styled.div`
+  height: 100%;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+  background-color: ${({ theme: { color } }) => color.mainYellow};
+  padding: 0 10px;
+`;
+
+export const StyledInputText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  font-size: ${({ theme: { fontSize } }) => fontSize.xxl};
+  font-weight: 100;
+
+  p {
+    margin: 0;
+    padding-bottom: 5px;
+  }
+`;
+
+export const StyledInput = styled(Input.Search)`
+  input {
+    width: 400px;
+    height: 40px;
+    border-radius: 0;
+    border: 0;
+  }
+
+  button {
+    width: 40px;
+    height: 40px;
+    margin-left: 2px;
+
+    &:hover {
+      border: 1;
+    }
+  }
+`;
