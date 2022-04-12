6f3156bb4012e3c8006096ab6c48550e5f259086
Krzysztof Kisielewski <90099871+kisielewskik@users.noreply.github.com>
2022-04-12 12:17:32 +0200

    Add main view (#3)
    * feat: home page
    
    * feat: input styles
    
    Co-authored-by: kisielewskik <k.kisielewski.k@gmail.com>

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,7 +1,29 @@
+import { Hero } from 'components/common';
 import React from 'react';
+import {
+  HomeWrapper,
+  InputWrapper,
+  StyledInput,
+  StyledInputText,
+} from './home.styled';
 
 const Home = () => {
-  return <div>Sweet home alabama</div>;
+  return (
+    <HomeWrapper>
+      <InputWrapper>
+        <StyledInputText>
+          <p>Time for some food</p>
+        </StyledInputText>
+        <StyledInput
+          onSearch={(value) => console.log(value)}
+          size="large"
+          placeholder="Enter your address"
+          enterButton
+        />
+      </InputWrapper>
+      <Hero />
+    </HomeWrapper>
+  );
 };
 
 export default Home;
