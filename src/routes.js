6eac6232c50bbb814a2383e94c353e7774516e7e
kisielewskik <k.kisielewski.k@gmail.com>
2022-04-19 10:37:14 +0200

    feat: common views

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 import { App, NotFound } from 'components/layouts';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from 'views';
+import { ContactUs, Help, Home, HowItWorks } from 'views';
 
 const Routing = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
-        <Route path="contact" element={<div>contact us</div>} />
-        <Route path="how-it-works" element={<div>how it works</div>} />
-        <Route path="help" element={<div>HELP</div>} />
+        <Route path="contact" element={<ContactUs />} />
+        <Route path="how-it-works" element={<HowItWorks />} />
+        <Route path="help" element={<Help />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
